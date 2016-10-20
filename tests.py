528a19a0f7185dc5b925de2acff41056b0aeac64
Jannis Gebauer <ja.geb@me.com>
2016-10-20 11:44:01 +0200

    added test that checks if the data is in sync

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 import unittest
 import json
 from packaging.specifiers import SpecifierSet, InvalidSpecifier
-from packaging.specifiers import InvalidSpecifier
 
 
 class TestData(unittest.TestCase):
@@ -20,7 +19,19 @@ class TestData(unittest.TestCase):
                 self.fail("No valid json in insecure_full.json: {}".format(e),)
 
     def test_data_in_sync(self):
-        self.assertEquals(self.db.keys(), self.db_full.keys())
+        # test all packages are in sync
+        for key in self.db.keys():
+            self.assertTrue(key in self.db_full.keys())
+
+        # test all pkg versions in the small db are also in the full db
+        for pkg in self.db:
+            db_versions = set(sorted(self.db[pkg]))
+            db_full_versions = set(sorted(item["v"] for item in self.db_full[pkg]))
+            self.assertEquals(
+                db_versions,
+                db_full_versions,
+                "Versions for {} not matching\n{}\n{}".format(pkg, db_versions, db_full_versions)
+            )
 
     def test_using_valid_specifier_sets(self):
 
@@ -37,8 +48,6 @@ class TestData(unittest.TestCase):
         for values in self.db_full.values():
             for item in values:
                 test_spec(item["v"], "insecure.json")
-
-
 
 
 if __name__ == '__main__':
