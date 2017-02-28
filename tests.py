263508725c9adf2f0e1b3b1b78c0b1b6337b6262
Jannis Gebauer <ja.geb@me.com>
2017-02-28 16:02:15 +0100

    remove unused test

--- tests.py
+++ tests.py
@@ -12,27 +12,6 @@ class TestData(unittest.TestCase):
             except ValueError as e:
                 self.fail("No valid json in insecure.json: {}".format(e),)
 
-        with open("data/insecure_full.json") as f:
-            try:
-                self.db_full = json.loads(f.read())
-            except ValueError as e:
-                self.fail("No valid json in insecure_full.json: {}".format(e),)
-
-    def test_data_in_sync(self):
-        # test all packages are in sync
-        for key in self.db.keys():
-            self.assertTrue(key in self.db_full.keys())
-
-        # test all pkg versions in the small db are also in the full db
-        for pkg in self.db:
-            db_versions = set(sorted(self.db[pkg]))
-            db_full_versions = set(sorted(item["v"] for item in self.db_full[pkg]))
-            self.assertEqual(
-                db_versions,
-                db_full_versions,
-                "Versions for {} not matching\n{}\n{}".format(pkg, db_versions, db_full_versions)
-            )
-
     def test_using_valid_specifier_sets(self):
 
         def test_spec(spec, db):
@@ -44,11 +23,6 @@ class TestData(unittest.TestCase):
         for specifiers in self.db.values():
             for specifier in specifiers:
                 test_spec(specifier, "insecure.json")
-
-        for values in self.db_full.values():
-            for item in values:
-                test_spec(item["v"], "insecure.json")
-
 
 if __name__ == '__main__':
     unittest.main()
