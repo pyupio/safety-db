e365bb4ecc5a237179e123f81f4be5b23c0cc412
Andrew White <55465692+ajw-aws@users.noreply.github.com>
2020-05-25 09:10:35 -0700

    Access files as setuptools pkg_resource

---
+++ __init__.py
@@ -0,0 +1,25 @@
+import json
+from pkg_resources import resource_filename
+
+__title__ = 'safety-db'
+__version__ = '2020.4.14'
+__author__ = 'Justin Womersley <support@pyup.io>'
+__copyright__ = '2016-2017 PyUp'
+__license__ = 'Attribution-NonCommercial-ShareAlike 4.0 International'
+__all__ = (
+    'INSECURE',
+    'INSECURE_FULL',
+)
+
+with open(resource_filename("safety_db", "insecure.json")) as __f:
+    try:
+        INSECURE = json.loads(__f.read())
+    except ValueError as e:
+        INSECURE = []
+
+
+with open(resource_filename("safety_db", "insecure_full.json")) as __f:
+    try:
+        INSECURE_FULL = json.loads(__f.read())
+    except ValueError as e:
+        INSECURE_FULL = []
